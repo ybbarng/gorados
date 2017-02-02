2676d2fd3850157e7acce261e0310e84d44f6e87
ybbarng <ybbarng@gmail.com>
2017-02-02 15:03:43 +0900

    값이 변하지 않도록 수집하는 최대 크기 찾음
    20

--- get_data.py
+++ get_data.py
@@ -1,4 +1,10 @@
+import time
+import json
+
 import requests
+
+import jsonify
+
 
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.0; en-US) AppleWebKit/525.13 (KHTML, like Gecko) Chrome/0.2.149.29 Safari/525.13',
@@ -16,29 +22,85 @@ def get(url, header=None, params=None, encoding='utf-8', print_url=False, cookie
     res.encoding = encoding
     return res.text
 
+test = {
+    'lat_start': 37.428388,
+    'lat_end': 37.528388,
+    'lng_start': 126.764500,
+    'lng_end': 126.864500,
+}
+
+snu = {
+    'lat_start': 37.4612113,
+    'lat_end': 37.4683902,
+    'lng_start': 126.9494231,
+    'lng_end': 126.9620587,
+}
+
+seoul = {
+    'lat_start': 37.428388,
+    'lat_end': 40.701417,
+    'lng_start': 126.764500,
+    'lng_end': 127.183760
+}
+
+test = {
+    'lat_start': 37.5,
+    'lat_end': 37.6,
+    'lng_start': 126.7,
+    'lng_end': 126.8
+}
+
 
 url = 'http://www2.pokemonsmap.com/map/scripts/getMarkers.php'
 params = {
     'callback': 'jQuery22006645514662215135_1486001171505',
     'cmd': 'sp3',
-    'lat_start': 37.428388,
-    'lat_end': 37.701417,
-    'lng_start': 126.764500,
-    'lng_end': 127.183760,
     '_': 1486001171
 }
-'''
-params = {
-    'callback': 'jQuery22006645514662215135_1486001171505',
-    'cmd': 'sp3',
-    'lat_start': 37.4612113,
-    'lat_end': 37.4683902,
-    'lng_start': 126.9494231,
-    'lng_end': 126.9620587,
-    '_': 1486001171
-}
-'''
 
-with open('data.json', 'w') as f:
-    f.write(get(url, params=params))
+region = test
+print(region)
 
+precision = 20
+lat_start = int(region['lat_start'] * precision)
+lat_end = int(region['lat_end'] * precision) + 1
+lng_start = int(region['lng_start'] * precision)
+lng_end = int(region['lng_end'] * precision) + 1
+
+print('# of regions: {}'.format((lat_end - lat_start) * (lng_end - lng_start)))
+
+data1 = {}
+for i in range(lat_start, lat_end):
+    for j in range(lng_start, lng_end):
+        location = {
+            'lat_start': i / precision,
+            'lat_end': (i + 1) / precision,
+            'lng_start': j / precision,
+            'lng_end': (j + 1) / precision,
+        }
+        print(location)
+        for key, value in location.items():
+            params[key] = value
+        response = get(url, params=params)
+        data = jsonify.jsonify(response)
+        data1['{}-{}'.format(location['lat_start'], location['lng_start'])] = len(data)
+        time.sleep(1)
+
+data2 = {}
+for i in range(lat_start, lat_end):
+    for j in range(lng_start, lng_end):
+        location = {
+            'lat_start': i / precision,
+            'lat_end': (i + 1) / precision,
+            'lng_start': j / precision,
+            'lng_end': (j + 1) / precision,
+        }
+        print(location)
+        for key, value in location.items():
+            params[key] = value
+        response = get(url, params=params)
+        data = jsonify.jsonify(response)
+        key = '{}-{}'.format(location['lat_start'], location['lng_start'])
+        if data1[key] != len(data):
+            print('{}: {} != {}'.format(key, data1[key], len(data)))
+        time.sleep(1)
--- prettify_data.py
+++ prettify_data.py
@@ -1,10 +1,14 @@
 import json
+import jsonify
 
-with open('data_origin.json', 'r') as f:
+with open('response.txt', 'r') as f:
     f.readline()
-    data = f.readline()
-    data = data.replace('jQuery22006645514662215135_1486001171505(', '').replace(')', '')
-    data = json.loads(data)
+    response = f.readline()
+
+data = jsonify.jsonify(response)
+data = {
+    'data': data
+}
 
 with open('data.json', 'w') as f:
     json.dump(data, f, indent='    ')
