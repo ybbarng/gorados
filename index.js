449d18e1184c4ed8e947fbb21b8c7b0184293721
ybbarng <ybbarng@gmail.com>
2017-02-27 21:54:59 +0900

    지도 중심을 기준으로 가까운 것부터 500개를 리턴하게 함

--- index.js
+++ index.js
@@ -9,13 +9,27 @@ var app = express();
 
 app.use(express.static(__dirname + '/app'));
 
+function get_center(req) {
+  return {
+    latitude: (parseFloat(req.query.max_latitude) + parseFloat(req.query.min_latitude)) / 2,
+    longitude: (parseFloat(req.query.max_longitude) + parseFloat(req.query.min_longitude)) / 2,
+  };
+}
+
 app.get('/places.json', function(req, res) {
+  var center = get_center(req);
   db.all('SELECT * FROM ' + place_table +
-    ' WHERE latitude >= ? AND latitude < ? AND longitude >= ? AND longitude < ?' + ' LIMIT 500',
+    ' WHERE latitude >= ? AND latitude < ? AND longitude >= ? AND longitude < ?' +
+    ' ORDER BY ((latitude - ?) * (latitude - ?) + (longitude - ?) * (longitude - ?)) ASC' +
+    ' LIMIT 500',
     req.query.min_latitude,
     req.query.max_latitude,
     req.query.min_longitude,
     req.query.max_longitude,
+    center.latitude,
+    center.latitude,
+    center.longitude,
+    center.longitude,
     function(err, rows) {
       res.send(rows);
   });
@@ -23,13 +37,20 @@ app.get('/places.json', function(req, res) {
 
 app.get('/pokemons.json', function(req, res) {
   var timestamp = Date.now() / 1000 | 0;
-  db.all('SELECT * FROM ' + pokemon_table + 
-    ' WHERE latitude >= ? AND latitude < ? AND longitude >= ? AND longitude < ? AND despawn > ?' + ' LIMIT 500',
+  var center = get_center(req);
+  db.all('SELECT * FROM ' + pokemon_table +
+    ' WHERE latitude >= ? AND latitude < ? AND longitude >= ? AND longitude < ? AND despawn > ?' +
+    ' ORDER BY ((latitude - ?) * (latitude - ?) + (longitude - ?) * (longitude - ?)) ASC' +
+    ' LIMIT 500',
     req.query.min_latitude,
     req.query.max_latitude,
     req.query.min_longitude,
     req.query.max_longitude,
     timestamp,
+    center.latitude,
+    center.latitude,
+    center.longitude,
+    center.longitude,
     function(err, rows) {
       res.send(rows);
   });
