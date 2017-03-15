383381fb71c87c181d1813de0d6e98dbf52f6187
ybbarng <ybbarng@gmail.com>
2017-03-15 13:20:48 +0900

    winston을 이용해서 서버 로그를 기록하게 함

--- index.js
+++ index.js
@@ -3,6 +3,40 @@ var compression = require('compression');
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('data.db');
 var classification = require('./classification.json');
+var present = require('present');
+var winston = require('winston');
+require('winston-daily-rotate-file');
+
+var timeOffset = 9 * 60 * 60 * 1000;
+
+function timestamp() {
+  return new Date(Date.now() + timeOffset).toISOString();
+}
+
+function formatter(options) {
+  return '[' + options.timestamp() + '] ' +
+    options.level.toUpperCase() + ' | ' +
+    (options.message ? options.message : '' ) +
+    (options.meta && Object.keys(options.meta).length ? '\n\t'+ JSON.stringify(options.meta) : '' );
+}
+
+var logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      level: 'info',
+      timestamp: timestamp,
+      formatter: formatter
+    }),
+    new winston.transports.DailyRotateFile({
+      filename: './log/debug-',
+      datePattern: 'yyyy-MM-dd.log',
+      level: 'debug',
+      json: false,
+      timestamp: timestamp,
+      formatter: formatter
+    })
+  ]
+});
 
 var place_table = 'place';
 var pokemon_table = 'pokemon';
@@ -24,6 +58,8 @@ function get_center(req) {
 
 app.get('/places.json', function(req, res) {
   var center = get_center(req);
+  var startTime = present();
+  logger.log('debug', 'Places query is requested to the DB.');
   db.all('SELECT * FROM ' + place_table +
     ' WHERE latitude >= ? AND latitude < ? AND longitude >= ? AND longitude < ?' +
     ' ORDER BY ((latitude - ?) * (latitude - ?) + (longitude - ?) * (longitude - ?)) ASC' +
@@ -37,6 +73,9 @@ app.get('/places.json', function(req, res) {
     center.longitude,
     center.longitude,
     function(err, rows) {
+      var endTime = present();
+      logger.log('debug', 'The response of Places query is received from the DB.');
+      logger.log('info', 'Places query time spent: %d ms', endTime - startTime);
       res.send(rows);
   });
 });
@@ -52,7 +91,7 @@ app.get('/pokemons.json', function(req, res) {
   }
   var zoom_level = parseInt(req.query.zoom_level) - 12;
   zoom_level = Math.min(Math.max(0, zoom_level), 4);
-  console.log('Zoom Level: ' + req.query.zoom_level + ', ' + 'Classification Index: ' + zoom_level);
+  logger.log('info', 'Zoom Level: %s, Classification Index: %s', req.query.zoom_level, zoom_level);
   var pokemons = classification[zoom_level];
   if (req.query.filters) {
     if (/^[\d,]+$/.test(req.query.filters)) {
@@ -63,6 +102,8 @@ app.get('/pokemons.json', function(req, res) {
   }
   var timestamp = Date.now() / 1000 | 0;
   var center = get_center(req);
+  var startTime = present();
+  logger.log('debug', 'Pokemons query is requested to the DB.');
   db.all('SELECT *' +
     ' FROM ' + pokemon_table +
     ' INDEXED BY pokemon_despawn_idx' +
@@ -80,6 +121,9 @@ app.get('/pokemons.json', function(req, res) {
     center.longitude,
     center.longitude,
     function(err, rows) {
+      var endTime = present();
+      logger.log('debug', 'The response of Pokemons query is received from the DB.');
+      logger.log('info', 'Pokemons query time spent: %d ms', endTime - startTime);
       res.send(rows);
   });
 });
@@ -92,22 +136,28 @@ app.get('/pokemon.json', function(req, res) {
     res.send([]);
     return;
   }
+  var startTime = present();
+  logger.log('debug', 'Pokemon query is requested to the DB.');
   db.all('SELECT *' +
     ' FROM ' + pokemon_table +
     ' WHERE id = ?',
     id,
     function(err, rows) {
+      var endTime = present();
+      logger.log('debug', 'The response of Pokemon query is received from the DB.');
+      logger.log('info', 'Pokemon query time spent: %d ms', endTime - startTime);
       res.send(rows);
   });
 });
 
 var port = 12026;
 app.listen(port, function() {
-  console.log('Server is started.');
-  console.log('Listening on ' + port);
+  logger.log('info', 'Server is started.');
+  logger.log('info', 'Listening on %d', port);
 });
 
 process.on('SIGINT', function() {
+  logger.log('info', 'SIGINT is received. Server is stopped.');
   db.close();
   process.exit();
 });
